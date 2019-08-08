64daa15f3dc3c54061f71a6ac2d5bd9d1553bae8
Theta <tanmya2000@gmail.com>
2019-08-07 20:19:18 -0700

    Add files via upload

---
+++ diffsum_cc.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Tanmya
+#
+# Created:     04/08/2019
+# Copyright:   (c) Tanmya 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input())
+b=int(input())
+
+if a>b:
+    print(a-b)
+else:
+    print(a+b)
